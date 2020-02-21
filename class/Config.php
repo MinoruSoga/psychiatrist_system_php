b88e8c280d749c4e7edc49ed9545153d623975ae
MinoruSoga <minoru.2021@icloud.com>
2020-02-21 18:38:08 +0900

    heroku

--- class/Config.php
+++ class/Config.php
@@ -7,11 +7,21 @@ class Config
     private $username = "root";
     private $password = "root";
     private $database = "psychiatrist_system";
-
     public $conn;
 
     public function __construct()
     {
+        $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+        $this->servername = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+        $db['dbname'] = ltrim($db['path'], '/');
+        $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+        $this->username = $db['user'];
+        $this->password = $db['pass'];
+        $this->database = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+          );
         //Create the connection
         $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
 
